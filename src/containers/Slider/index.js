6f5a4cea80dbfbf5a051f872832b70f9a9ff06de
Boca75 <arthur@uvnet.fr>
2023-10-04 12:49:18 +0200

    fix error

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -33,42 +33,40 @@ const Slider = () => {
         <div className="SlideCardList">
             {byDateDesc &&
                 byDateDesc.map((event, idx) => (
-                    <>
-                        <div
-                            key={event.id}
-                            className={`SlideCard SlideCard--${
-                                index === idx ? "display" : "hide"
-                            }`}
-                        >
-                            <img src={event.cover} alt="forum" />
-                            <div className="SlideCard__descriptionContainer">
-                                <div className="SlideCard__description">
-                                    <h3>{event.title}</h3>
-                                    <p>{event.description}</p>
-                                    <div>{getMonth(new Date(event.date))}</div>
-                                </div>
+                    <div
+                        key={event.id}
+                        className={`SlideCard SlideCard--${
+                            index === idx ? "display" : "hide"
+                        }`}
+                    >
+                        <img src={event.cover} alt="forum" />
+                        <div className="SlideCard__descriptionContainer">
+                            <div className="SlideCard__description">
+                                <h3>{event.title}</h3>
+                                <p>{event.description}</p>
+                                <div>{getMonth(new Date(event.date))}</div>
                             </div>
                         </div>
-                        <div className="SlideCard__paginationContainer">
-                            <div className="SlideCard__pagination">
-                                {byDateDesc &&
-                                    byDateDesc.map((radio, radioIdx) => (
-                                        <input
-                                            key={radio.id}
-                                            type="radio"
-                                            name="radio-button"
-                                            checked={index === radioIdx}
-                                            onChange={() =>
-                                                handleBulletClick(radioIdx)
-                                            }
-                                        />
-                                    ))}
-                            </div>
-                        </div>
-                    </>
+                    </div>
                 ))}
+            <div className="SlideCard__paginationContainer">
+                <div className="SlideCard__pagination">
+                    {byDateDesc &&
+                        byDateDesc.map((radio, radioIdx) => (
+                            <input
+                                key={radio.id}
+                                type="radio"
+                                name="radio-button"
+                                checked={index === radioIdx}
+                                onChange={() => handleBulletClick(radioIdx)}
+                            />
+                        ))}
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Slider;
+
+

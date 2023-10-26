8076853ed4e8f526463baeb1338a6c0718b76360
Boca75 <arthur@uvnet.fr>
2023-10-26 17:36:52 +0200

    fix integration test

--- src/pages/Home/index.integration.test.js
+++ src/pages/Home/index.integration.test.js
@@ -1,40 +1,37 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "./index";
 
-// import { fireEvent, render, screen } from "@testing-library/react";
-// import Home from "./index";
+describe("Integration Tests for Home Page", () => {
+    it("should render the Home page correctly", () => {
+        render(<Home />);
 
-// describe("Integration Tests for Home Page", () => {
-//     it("should render the Home page correctly", () => {
-//         render(<Home />);
+        // Assertions for the Home page
+        expect(screen.getAllByText("Nos services")).toBeTruthy();
+        expect(screen.getAllByText("Nos réalisations")).toBeTruthy();
+        expect(screen.getAllByText("Notre équipe")).toBeTruthy();
+        expect(screen.getAllByText("Contact")).toBeTruthy();
+    });
 
-//         // Assertions for the Home page
-//         expect(screen.getAllByText("Nos services")).toBeInTheDocument();
-//         expect(screen.getAllByText("Nos réalisations")).toBeInTheDocument();
-//         expect(screen.getAllByText("Notre équipe")).toBeInTheDocument();
-//         expect(screen.getAllByText("Contact")).toBeInTheDocument();
-//     });
+    it("should simulate form submission and display success message", async () => {
+        render(<Home />);
 
-//     it("should simulate form submission and display success message", async () => {
-//         render(<Home />);
+        // Simulate form submission
+        fireEvent.click(screen.getByText("Envoyer"));
 
-//         // Simulate form submission
-//         fireEvent.click(screen.getByText("Envoyer"));
+        // Check if the "En cours" message is displayed
+        expect(await screen.findByText("En cours")).toBeTruthy();
 
-//         // Check if the "En cours" message is displayed
-//         expect(await screen.findByText("En cours")).toBeInTheDocument();
+        // Check if the "Message envoyé !" success message is displayed
+        expect(await screen.findByText("Message envoyé !")).toBeTruthy();
+    });
 
-//         // Check if the "Message envoyé !" success message is displayed
-//         expect(await screen.findByText("Message envoyé !")).toBeInTheDocument();
-//     });
+    it("should verify the footer", () => {
+        render(<Home />);
 
-//     it("should verify the footer", () => {
-//         render(<Home />);
-
-//         // Check if footer elements are displayed
-//         expect(
-//             screen.getByText("Notre dernière prestation")
-//         ).toBeInTheDocument();
-//         expect(screen.getByText("Contactez-nous")).toBeInTheDocument();
-//     });
-// });
+        // Check if footer elements are displayed
+        expect(screen.getByText("Notre dernière prestation")).toBeTruthy();
+        expect(screen.getByText("Contactez-nous")).toBeTruthy();
+    });
+});
 
 
